c9fc8bb3b10bda3356610b30f32e7fcbd127f204
김도훈 <dohun0310@naver.com>
2025-11-17 17:25:49 +0900

    [Design/#1] Chore: `Nanum_Gothic` 폰트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,20 @@
 import type { Metadata } from "next";
+import { Nanum_Gothic } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+
+const nanumGothic = Nanum_Gothic({
+  display: "swap",
+  weight: ["400", "700", "800"],
+  variable: "--font-nanum-gothic",
+});
 
 const pretendard = localFont({
   src: "../node_modules/pretendard/dist/web/variable/woff2/PretendardVariable.woff2",
   display: "swap",
   weight: "45 920",
   variable: "--font-pretendard",
-})
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${pretendard.variable} antialiased`}
+        className={`${pretendard.variable} ${nanumGothic.variable} antialiased`}
       >
         {children}
       </body>
