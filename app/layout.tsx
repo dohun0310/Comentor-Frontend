d81609ab87814d3f5588025a4b0c963be1dfc8e0
김도훈 <dohun0310@naver.com>
2025-11-23 20:36:35 +0900

    Chore: HTML 언어 속성을 'ko'로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
         className={`${pretendard.variable} ${nanumGothic.variable} antialiased`}
       >
